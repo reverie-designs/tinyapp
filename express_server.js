4701ee5275c00cd7e6193521456f8323236e1606
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-28 21:36:09 +0000

    added a urls_show.ejs that shows the long url at the promt of a short url - added new path in express server to handle short urls

--- express_server.js
+++ express_server.js
@@ -21,7 +21,12 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   let templateVars = {urls: urlDatabase}  ;
-  res.render('urls_index', templateVars);
+  res.render('urls_index.ejs', templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
