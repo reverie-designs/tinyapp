c9b89b0c8cb9c26a869781961a252bcda00a66da
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-28 20:49:29 +0000

    added EJS as a templating dependency

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
+//express_server.js
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set('view engine', 'ejs'); //sets ejs as the view engine - templating engine
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
