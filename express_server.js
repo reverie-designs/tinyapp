2cbb60e4c754db1d8eee9a19e2f62b3471791404
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-10-30 22:28:15 +0000

    passed registered user name to username

--- express_server.js
+++ express_server.js
@@ -2,22 +2,29 @@
 
 const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser'); //assists with cookies
+cookieParser = require('cookie-parser')
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser"); //translates post data
 // const methodOverride = require("method-override");
+
+//decodes post data from buffer into string
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(cookieParser());
+//uses method override to conver post to put
+// app.use(methodOverride('_method'));
+
+//sets ejs as the view engine - templating engine
+app.set('view engine', 'ejs'); 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-//This function was taken from:
-// https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript/9719815 
-// it generates an 8 character random string
-const randomString = function (){
+//This function was taken from https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript/9719815 - generates an 8 character rrandom string
+var randomString = function (){
   return Math.random().toString(36).slice(-8);
-};
+}
 
 //Will store user registration ===================================
 let users = {
@@ -33,7 +40,6 @@ let users = {
   }
 };
 
-
 //does registration email already exist?
 
 const emailExists = (email) => {
@@ -46,42 +52,37 @@ const emailExists = (email) => {
       return true;
     }
   }
-  return false;
 };
 
-//decodes post data from buffer into string
-app.use(bodyParser.urlencoded({extended: true})); 
-app.use(cookieParser());
-
-//uses method override to conver post to put
-// app.use(methodOverride('_method'));
-
-//sets ejs as the view engine - templating engine
-app.set('view engine', 'ejs'); 
 
 //redirects home page to /urls
 app.get("/", (req, res) => {
+  // res.send("Hello!");
   res.redirect("/urls/");
 });
 
 
-// displays summary of current short and long urls in your database
+
+//summary of current short and long urls in your database
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase, username: req.cookies.userId};
+  let templateVars = {urls: urlDatabase, user: users[req.cookies.userId]};
   res.render('urls_index', templateVars);
 });
 
 //gets registration page
 app.get("/urls_register", (req, res) => {
-  let templateVars = {urls: urlDatabase, username: req.cookies.userID};
+  console.log(users[req.cookies.userId]);
+  let templateVars = {urls: urlDatabase, user: users[req.cookies.userId]};
   res.render('urls_register', templateVars);
 });
 
 //adding new user
 app.post('/register', (req, res) => {
+  //no emtpy strings
   if (req.body.email === "" || req.body.password === "") {
     res.send('Please fill in both fields in order to register');
     res.sendStatus(400);  
+  //cannot have user with same email address
   } else if (emailExists(req.body.email)) {
     console.log('============ ', req.body.email);
     res.send('We already have a user registered with that email address');
@@ -89,13 +90,17 @@ app.post('/register', (req, res) => {
   } else {
     const userId = randomString();
     users[userId] = {id: userId, email: req.body.email, password: req.body.password};
-    res.cookie('userID', userId);
+    res.cookie('userId', userId);
     res.redirect('/urls');
   }
 });
 
 //login get cookies
 app.post("/login", (req, res) => {
+  console.log("Someone tried to sign in");
+  // console.log(req.body.username);
+  console.log(req.cookies.username);
+  // templateVars.username = req.body.username;
   res.cookie('username', req.body.username);
   res.redirect("/urls");
 });
@@ -106,8 +111,12 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+// let templateVars = {
+//   username: req.cookies["username"]
+// };
 //make new tiny url page
 app.get("/urls/new", (req, res) => { 
+  // console.log(templateVars.username);
   res.render("urls_new", {username: req.cookies.username});
 });
 
@@ -143,6 +152,7 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 //delete a short url and redirect to main page
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
+  // console.log(req.params.shortURL);
   res.redirect('/urls');
 });
 
@@ -154,5 +164,3 @@ app.get("/urls.json", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-//need to add code for edge cases where the short code doesn't exist
