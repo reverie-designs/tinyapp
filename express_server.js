aed575cab514cefc31dcd42b9940c6b04791ce89
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-02 16:38:12 +0000

    adjusted login and registration error responses

--- express_server.js
+++ express_server.js
@@ -1,14 +1,14 @@
 //express_server.js
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 3000; // default port 8080
 // const cookieParser = require('cookie-parser')//==============NEED TO UNINSTALL+methodOverride(
 const cookieSession = require('cookie-session');
 const bodyParser = require("body-parser"); //translates post data
 const bcrypt = require('bcrypt');
 
 //decodes post data from buffer into string
-app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ['magic', 'words'],
@@ -16,25 +16,25 @@ app.use(cookieSession({
 // app.use(cookieParser());
 
 //sets ejs as the view engine - templating engine
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
-  b6UTxQ: { 
-    longURL: "https://www.tsn.ca", 
-    userID: 'radomID2' 
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: 'radomID2'
   },
 };
 
 //This function was taken from https://stackoverflow.com/questions/9719570/generate-random-password-string-with-requirements-in-javascript/9719815 - generates an 8 character rrandom string
-var randomString = function (){
+const randomString = function() {
   return Math.random().toString(36).slice(-8);
-}
+};
 
 //Will store user registration ===================================
 let users = {
   'radomId': {
     id: 'radomID',
-    email: 'email',
+    email: 'email@gma.com',
     password: 'password',
   },
   'radomID2': {
@@ -44,9 +44,16 @@ let users = {
   }
 };
 
+const errors = {
+  registrationEmail: 'Sorry, we already have user registered with that email address.',
+  loginEmail: 'Sorry, we could not find a user registered with thtat email address.',
+  password: 'The password doesn\'t match the user email, please try again.',
+  bothFields: 'Please fill in both fields in order to register',
+  noURLS: 'Sorry we don\'t have any urls that match your request',
+}
 
 const getUserByEmail = (email) => {
-  const userIds = Object.keys(users)
+  const userIds = Object.keys(users);
   for (let userId of userIds) {
     if (users[userId].email === email) {
       return userId;
@@ -57,7 +64,7 @@ const getUserByEmail = (email) => {
 const getURLSByUserId = (userID) => {
   const urlIDs = Object.keys(urlDatabase);
   let userUrls = {};
-  if (userID){
+  if (userID) {
     urlIDs.forEach((item) => {
       if (urlDatabase[item].userID === userID) {
         urlDatabase[item].shortURL = item;
@@ -92,12 +99,12 @@ app.get("/urls/register", (req, res) => {
 app.post('/register', (req, res) => {
   //no emtpy strings
   if (req.body.email === "" || req.body.password === "") {
-    res.send('Please fill in both fields in order to register');
-    res.sendStatus(400);  
+    res.send(errors.bothFields.bold());
+    res.status(400);
   //cannot have user with same email address
   } else if (getUserByEmail(req.body.email)) {
-    res.send('We already have a user registered with that email address');
-    res.sendStatus(400); 
+    res.status(400);
+    res.send(errors.registrationEmail.bold());
   } else {
     const userId = randomString();
     const textPassword = req.body.password;
@@ -118,18 +125,19 @@ app.get("/urls/login", (req, res) => {
 app.post("/login", (req, res) => {
   const email = req.body.email;
   const userId = getUserByEmail(email);
-  if (!userId){
-    res.send('Could not find user with that email');
-    res.sendStatus(403);
+  if (!userId) {
+    res.status(403); 
+    res.send(errors.loginEmail.bold());
   } else {
     const password = req.body.password;
     let userPassword = users[userId].password;
     if (bcrypt.compareSync(password, userPassword)) {
       req.session.userId = userId;
-      res.redirect("/urls");   
+      res.redirect("/urls");
     } else {
-      res.send('The password doesn\'t match the provided email');
-      res.sendStatus(403);
+      res.status(403);
+      res.send(errors.password.bold())
+      // res.redirect("urls_errors", error);
     }
   }
 });
@@ -141,9 +149,9 @@ app.post("/logout", (req, res) => {
 });
 
 //make new tiny url page
-app.get("/urls/new", (req, res) => { 
+app.get("/urls/new", (req, res) => {
   let templateVars = {user: users[req.session.userId], };
-  if (templateVars.user){
+  if (templateVars.user) {
     res.render("urls_new", templateVars);
   } else {
     res.redirect("/urls/login");
@@ -155,18 +163,18 @@ app.get("/u/:shortURL", (req, res) => {
   let urlID = req.params.shortURL;
   const shortURL = urlDatabase[urlID];
   const longURL = shortURL.longURL;
-  if (longURL){
+  if (longURL) {
     res.redirect(longURL);
   } else {
     res.status(404);
-    res.send('Sorry we don\'t have any urls that match your request');
+    res.send(errors.noURLS.bold());
   }
 });
 
 //creating a new tiny url and adding it to the urlDatabase
 app.post("/urls", (req, res) => {
   let shortURL = randomString();
-  let longURL = req.body.longURL
+  let longURL = req.body.longURL;
   let userId = req.session.userId;
   urlDatabase[shortURL] = {'longURL': longURL, 'userID': userId};
   res.redirect(`/urls/${shortURL}`);
@@ -186,10 +194,11 @@ app.get("/urls/:ID", (req, res) => {
 
 //edit long url
 app.post("/urls/:shortURL/edit", (req, res) => {
-  if(req.session.userId){
-    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
+  let urls = getURLSByUserId(req.session.userId);
+  if (!req.session.userId || !urls) {
     res.redirect(`/urls`);
   } else {
+    urlDatabase[req.params.shortURL].longURL = req.body.longURL;
     res.redirect(`/urls`);
   }
   
@@ -197,7 +206,8 @@ app.post("/urls/:shortURL/edit", (req, res) => {
 
 //delete a short url and redirect to main page
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if(!req.session.userId){
+  let urls = getURLSByUserId(req.session.userId);
+  if (!req.session.userId || !urls) {
     res.redirect('/urls/login');
   } else {
     delete urlDatabase[req.params.shortURL];
